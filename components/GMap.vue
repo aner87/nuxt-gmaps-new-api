2d1d5def80567c66104081fb2d934af004ca3480
aner87 <andrea.concari@icloud.com>
2021-11-19 16:21:52 +0100

    test clusters 4

--- components/GMap.vue
+++ components/GMap.vue
@@ -135,7 +135,7 @@ export default {
         this.markerCluster = new MarkerClusterer({
           map,
           markers,
-          clusterOptions,
+          ...this.cluster.options,
         });
       }
     },
